1811c60cef6e5681a03f32e992cb4dd13993d341
Naresh Singh <nareshsingh0170@gmail.com>
2024-05-27 23:09:55 +0530

    Some Change

--- js/script.js
+++ js/script.js
@@ -2,26 +2,7 @@ window.addEventListener('load', function () {
   document.getElementById('nav').style.cssText = "left:0; transition:1.2s ease-in-out"
 })
 
-// window.addEventListener('scroll', function () {
-//   var header = document.getElementsByClassName('header');
-//   if (window.scrollY > 10) {
-//     header.classList.add('scrolled');
-//   } else {
-//     header.classList.remove('scrolled');
-//   }
-// });
-
-
-// window.addEventListener('scroll', function () {
-//   var header = document.getElementsByClassName('header'); // Selecting the first element with the class 'header'
-//   if (window.scrollY > 10) {
-//     header.classList.add('scrolled');
-//   } else {
-//     header.classList.remove('scrolled');
-//   }
-// });
-
-let header = document.querySelector(".header");
+let header = document.querySelector("#header");
 window.onscroll = function () {
   if (document.documentElement.scrollTop > 300) {
     header.classList.add("scrolled");
@@ -38,5 +19,22 @@ window.onscroll = function () {
   }
 }
 
-
 let hr = document.getElementById('hr').style.cssText = "transform: translateY(60px)";
+let value = 60;
+console.log(value);
+
+// window height on scroll
+window.addEventListener('scroll', () => {
+  if (document.documentElement.scrollTop >= 1100) {
+    document.getElementById('pp').style = "position: sticky";
+    document.getElementById('hr').style.cssText = `transform: translateY(${value--}px) transition: 1.5s ease-out`
+
+  }
+  
+  else if (document.documentElement.scrollTop <= 1100) {
+    document.getElementById('hr').style.cssText = `transform: translateY(${value}px) transition: 1.5s ease-out`;
+  }
+})
+
+
+
